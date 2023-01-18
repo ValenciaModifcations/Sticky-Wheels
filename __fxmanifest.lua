b917c14774854608e439d2d9bb87c0fdc649b464
ValenciaModifcations <122960130+ValenciaModifcations@users.noreply.github.com>
2023-01-18 02:47:09 -0500

    Update __fxmanifest.lua

--- __fxmanifest.lua
+++ __fxmanifest.lua
@@ -4,6 +4,8 @@ author 'Valencia Modifications' -- (Shawn V)
 description 'Simple script to make the wheels stay in the angle they were facing upon exit of vehicle.'
 version '1.2.3'
 
+-- Source https://github.com/ValenciaModifcations/Sticky-Wheels
+
 client_scripts {
     'client.lua'
 }
